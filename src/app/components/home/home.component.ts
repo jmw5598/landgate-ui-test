735a040cd67a75417b6208771208b85eeff36297
Jason White <jmw5598@gmail.com>
2020-09-24 23:52:10 -0400

    Themed home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeAnimation } from 'src/app/animations/fade.animation';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [fadeAnimation]
 })
 export class HomeComponent implements OnInit {
 
