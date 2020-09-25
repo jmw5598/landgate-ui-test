6be0a1f6442b41a5c64db56cffec2cd42946877d
Jason White <jmw5598@gmail.com>
2020-09-25 00:02:23 -0400

    Complete getPeople method for people service.  Also created updatePerson method.

--- src/app/services/people.service.ts
+++ src/app/services/people.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {delay} from 'rxjs/operators';
+import {delay, filter} from 'rxjs/operators';
 import {Observable, of} from 'rxjs';
 import {PersonModel} from '../models/person-model';
 
@@ -10,17 +10,27 @@ export class PeopleService {
 
   constructor() { }
 
-  private mockPeopleList = [
-    {firstName: 'John', lastName: 'Doe', age: '21', workTitle: 'Wanna be Signer'},
-    {firstName: 'Jane', lastName: 'Doe', age: '22', workTitle: 'Signer'},
-    {firstName: 'Bob', lastName: 'Barker', age: '80', workTitle: 'TV Host'},
-    {firstName: 'John', lastName: 'Doe', age: '21', workTitle: 'Wanna be Signer'},
+  private mockPeopleList: PersonModel[] = [
+    {id: 1, firstName: 'John', lastName: 'Doe', age: +'21', jobTitle: 'Wanna be Signer'},
+    {id: 2, firstName: 'Jane', lastName: 'Doe', age: +'22', jobTitle: 'Signer'},
+    {id: 3, firstName: 'Bob', lastName: 'Barker', age: +'80', jobTitle: 'TV Host'},
+    {id: 4, firstName: 'John', lastName: 'Doe', age: +'21', jobTitle: 'Wanna be Signer'},
   ];
 
 
   getPeople(): Observable<PersonModel[]> {
     // TODO: Finish this implementation using the data from mockPeopleList
     // of(true).pipe(delay(100))
-    return null;
+    return of(
+      this.mockPeopleList.map(e => ({ ...e }))
+    ).pipe(delay(100));
+  }
+
+  updatePerson(person: PersonModel): Observable<PersonModel> {
+    let existingPerson: PersonModel = this.mockPeopleList.find(e => e.id === person.id);
+    if (existingPerson) {
+      Object.assign(existingPerson, person);
+    }
+    return of({...existingPerson}).pipe(delay(100));
   }
 }
